aa310b7ca64c44449cf91f175b7e295df2554105
HuongLanTo <tohuonglan1705@gmail.com>
2024-06-19 21:06:53 -0500

    Create Movies Controller

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -1,4 +1,7 @@
 import express from "express";
+import MoviesController from "./movies.controller.js";
+
 const router = express.Router(); // get access to express router
-router.route("/").get((req, res) => res.send("Hello World"));
+router.route("/").get(MoviesController.apiGetMovies);
+
 export default router;

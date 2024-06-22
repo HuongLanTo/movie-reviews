1b270e33cef95f1155d5483c8e166f15abc96945
HuongLanTo <tohuonglan1705@gmail.com>
2024-06-22 16:46:09 -0500

    APIs: Get single movie & Get movies' ratings

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -9,5 +9,7 @@ router
   .post(ReviewsController.apiPostReview)
   .put(ReviewsController.apiUpdateReview)
   .delete(ReviewsController.apiDeleteReview);
+router.route("/id/:id").get(MoviesController.apiGetMovieById);
+router.route("/ratings").get(MoviesController.apiGetRatings);
 
 export default router;

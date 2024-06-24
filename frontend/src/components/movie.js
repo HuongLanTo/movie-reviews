4e4c20c9ba987c88213ed384b1200494896d9fdf
HuongLanTo <tohuonglan1705@gmail.com>
2024-06-24 03:53:49 -0500

    Add reviews section in Movie component

--- frontend/src/components/movie.js
+++ frontend/src/components/movie.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import MovieDataService from "../services/movies.js";
+import moment from "moment";
 
-import { Link } from 'react-router-dom'
-import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container'; 
-import Image from 'react-bootstrap/Image';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
 
 const Movie = (props) => {
   const [movie, setMovie] = useState({
@@ -35,14 +37,14 @@ const Movie = (props) => {
 
   return (
     <div>
-      <Container>
+      <Container className="text-start">
         <Row>
-          <Col>
-            <Image src={movie.poster} width={"500px"} height={"100%"} fluid />
+          <Col className="me-5">
+            <Image src={movie.poster} width={"100%"} height={"100%"} fluid />
           </Col>
           <Col>
             <Card>
-              <Card.Header as="h5">{movie.title}</Card.Header>
+              <Card.Header as="h3" className="fw-bold">{movie.title}</Card.Header>
               <Card.Body>
                 <Card.Text>{movie.plot}</Card.Text>
                 {props.user && (
@@ -53,7 +55,39 @@ const Movie = (props) => {
               </Card.Body>
             </Card>
             <br></br>
-            <h2>Reviews</h2>
+            <h3 className="text-decoration-underline">Reviews:</h3>
+            {movie.reviews.map((review, index) => {
+              return (
+                <Card key={index} className="mb-3">
+                  <Card.Body>
+                    <h5>
+                      {review.name + " reviewed on "}{" "}
+                      {moment(review.date).format("Do MMM YYYY")}
+                    </h5>
+
+                    <p>{review.review}</p>
+                    {props.user && props.user.id === review.user_id && (
+                      <Row>
+                        <Col>
+                          <Link
+                            to={{
+                              pathname:
+                                "/movies/" + props.match.params.id + "/review",
+                              state: { currentReview: review },
+                            }}
+                          >
+                            Edit
+                          </Link>
+                        </Col>
+                        <Col>
+                          <Button variant="link">Delete</Button>
+                        </Col>
+                      </Row>
+                    )}
+                  </Card.Body>
+                </Card>
+              );
+            })}
           </Col>
         </Row>
       </Container>

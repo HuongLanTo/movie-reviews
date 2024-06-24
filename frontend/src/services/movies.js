a1e29d5f8ca0d61c300f9c949a98f38354017b4d
HuongLanTo <tohuonglan1705@gmail.com>
2024-06-23 23:03:48 -0500

    Connect to the backend

---
+++ frontend/src/services/movies.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const backendUrl = process.env.BACKEND_URL;
+
+class MovieDataService {
+  getAll = (page = 0) => {
+    return axios.get(`${backendUrl}?page=${page}`);
+  };
+
+  getMovie = (id) => {
+    return axios.get(`${backendUrl}/id/${id}`);
+  };
+
+  find = (query, by = "title", page = 0) => {
+    return axios.get(`${backendUrl}?${by}=${query}&page=${page}`);
+  };
+
+  createReview = (data) => {
+    return axios.post(`${backendUrl}/review`, data);
+  };
+
+  updateReview = (data) => {
+    return axios.put(`${backendUrl}/review`, data);
+  };
+
+  deleteReview = (id, userId) => {
+    return axios.delete(`${backendUrl}/review`, {
+      data: { review_id: id, user_id: userId },
+    });
+  };
+
+  getRatings = () => {
+    return axios.get(`${backendUrl}/ratings`);
+  };
+}
+
+export default new MovieDataService();
